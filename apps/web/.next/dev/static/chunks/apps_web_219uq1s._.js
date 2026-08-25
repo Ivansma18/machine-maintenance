4245909fa38c0e04cf666935b2ae95492702ec90
Ivansma18 <JIDiaz@sma.com.mx>
2026-08-25 10:58:07 -0600

    chore: add .next into gitignore

--- apps/web/.next/dev/static/chunks/apps_web_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_@ant-design_cssinjs_es_1ry-1yk._.js","static/chunks/node_modules_antd_17cpcms._.js","static/chunks/node_modules_@rc-component_trigger_es_09rpdu6._.js","static/chunks/node_modules_motion-dom_dist_es_0lc8d2_._.js","static/chunks/node_modules_1eyelnk._.js","static/chunks/apps_web_1w_gr6_._.js","static/chunks/apps_web_app_globals_1lkgh9-.css","static/chunks/apps_web_04lhpbk._.js","static/chunks/node_modules_framer-motion_dist_es_0j8rtby._.js","static/chunks/node_modules_antd_es_0zqm9zp._.js","static/chunks/node_modules_@rc-component_form_es_00nqys4._.js","static/chunks/node_modules_@rc-component_menu_es_02ews3f._.js","static/chunks/node_modules_0pnvlrb._.js"],
-    source: "entry"
-});
